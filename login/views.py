4bce24ae5f37fb32f0acb46bddff0b4fec49acf9
Angel <fisiangel.14@gmail.com>
2026-04-24 09:36:21 -0500

    add path for login

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-def login(request):
+def fisrtLogin(request):
     if request.user.is_authenticated:
         return redirect(request, 'home')
     
